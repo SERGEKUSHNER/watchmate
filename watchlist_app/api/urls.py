365fafd52c418e1a186f09bce69d370cb4122155
SERGEKUSHNER <kusnir.sergiu@gmail.com>
2021-09-19 20:26:42 +0300

    Step 9 -Concrete_ViewClasses + OverwriteQueryset

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
 import watchlist_app
-from watchlist_app.api.views import StreamPlatform, WatchList, WatchDetailAV, StreamPlatformAV, WatchListAV, \
-    StreamPlatformDetailAV, ReviewList, ReviewDetail
+from watchlist_app.api.views import (StreamPlatform, WatchList, WatchDetailAV, StreamPlatformAV, WatchListAV, \
+                                     StreamPlatformDetailAV, ReviewList, ReviewDetail, ReviewCreate)
 from watchlist_app.models import Review
 
 urlpatterns = [
@@ -11,6 +11,11 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
 
-    path('review', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail')
+    # path('review/', ReviewList.as_view(), name='review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail')
+
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='review-list'),
+    path('stream/<int:pk>/review', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+
 ]
