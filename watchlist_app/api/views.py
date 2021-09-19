9cce39e41e314ab68da715a77ac9f53c2fda63aa
SERGEKUSHNER <kusnir.sergiu@gmail.com>
2021-09-19 22:43:38 +0300

    Step 11 - Viewsets

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -67,6 +67,14 @@ class StreamPlatformVS(viewsets.ViewSet):
         serializer = StreamPlatformSerializer(watchlist)
         return Response(serializer.data)
 
+    def create(self, request):
+        serializer = StreamPlatformSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 class StreamPlatformAV(APIView):
     def get(self, request):
