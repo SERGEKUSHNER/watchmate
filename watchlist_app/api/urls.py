c1c14bbfcf1ce77bee0392bf68808aa27aa0f7e7
SERGEKUSHNER <kusnir.sergiu@gmail.com>
2021-09-19 15:38:40 +0300

    Step 8 - GenericAPIView

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,11 +2,15 @@ from django.urls import path, include
 
 import watchlist_app
 from watchlist_app.api.views import StreamPlatform, WatchList, WatchDetailAV, StreamPlatformAV, WatchListAV, \
-    StreamPlatformDetailAV
+    StreamPlatformDetailAV, ReviewList, ReviewDetail
+from watchlist_app.models import Review
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail')
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+
+    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail')
 ]
